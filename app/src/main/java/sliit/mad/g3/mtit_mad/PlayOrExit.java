afec665e1523ea0cb1157cce121f76d9b90bc0c0
vinuradeelaka <vinuraganegoda@gmail.com>
2017-06-03 14:07:47 +0530

    one interface and Java files1

--- app/src/main/java/sliit/mad/g3/mtit_mad/PlayOrExit.java
+++ app/src/main/java/sliit/mad/g3/mtit_mad/PlayOrExit.java
@@ -13,7 +13,7 @@ public class PlayOrExit extends Activity {
 
 
     ImageView btnExit,btnPlay;
-
+//------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
